4e2dd2271f1f6ebd3ce4a94f5b5fe7c3fdc0bf1e
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-09-06 09:11:32 +0200

    Debug

---
+++ scripts/test-process.mjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+// eslint-disable-next-line no-undef
+console.log('PROCESS.ENV:', JSON.stringify(process.env));
