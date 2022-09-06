dc7c85ddfe00f6169294284639a479c9e3e4deb3
Andrei Zhaleznichenka <zhalea@amazon.de>
2022-09-06 09:20:48 +0200

    Add prepare-package-lock to preinstall

---
+++ scripts/prepare-package-lock.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ * Remove specific @cloudscape-design/* packages where we should always use the latest minor release.
+ * Also checks for any dependencies that incorrectly use CodeArtifact instead of npm.
+ */
+
+if (process.env.GITHUB_WORKFLOW) {
+  const fs = require('fs');
+
+  const filename = require.resolve('../package-lock.json');
+  const packageLock = require(filename);
+
+  Object.keys(packageLock.dependencies).forEach(dependencyName => {
+    const dependency = packageLock.dependencies[dependencyName];
+    if (dependencyName.startsWith('@cloudscape-design/')) {
+      delete packageLock.dependencies[dependencyName];
+    } else if (dependency.resolved && dependency.resolved.indexOf('codeartifact.us-west-2.amazonaws.com') !== -1) {
+      throw Error(
+        'package-lock.json file contains a reference to CodeArtifact. Use regular npm to update the packages.'
+      );
+    }
+  });
+  fs.writeFileSync(filename, JSON.stringify(packageLock, null, 2) + '\n');
+  console.log('Removed @cloudscape-design/ dependencies from package-lock file');
+}
--- scripts/test-process.mjs
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-// eslint-disable-next-line no-undef
-console.log('PROCESS.ENV:', JSON.stringify(process.env));
