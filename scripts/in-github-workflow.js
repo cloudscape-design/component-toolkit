b3bc9676980495b691d156cbe1cb873b779e164a
Boris Serdiuk <serdiuk@amazon.com>
2022-11-29 16:02:09 +0100

    chore: Fix preinstall script and pre-commit hooks

--- scripts/in-github-workflow.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-if (process.env.GITHUB_WORKFLOW) {
-  process.exit(0);
-} else {
-  process.exit(1);
-}
