47431390b66f130044a6cb319aebb256b2cc0a66
Michael Dowse <dowsem@amazon.com>
2022-12-02 11:37:35 +0000

    chore: Upgrade to Node16
    - Regenerate package-lock
    - Upgrade dependencies

--- scripts/prepare-package-lock.js
+++ scripts/prepare-package-lock.js
@@ -2,21 +2,25 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+const fs = require('fs');
+
 /**
  * Remove specific @cloudscape-design/* packages where we should always use the latest minor release.
  * Also checks for any dependencies that incorrectly use CodeArtifact instead of npm.
  */
-
-const fs = require('fs');
-
 const filename = require.resolve('../package-lock.json');
 const packageLock = require(filename);
 
-Object.keys(packageLock.dependencies).forEach(dependencyName => {
-  const dependency = packageLock.dependencies[dependencyName];
-  if (dependencyName.startsWith('@cloudscape-design/')) {
-    delete packageLock.dependencies[dependencyName];
-  } else if (dependency.resolved && dependency.resolved.indexOf('codeartifact.us-west-2.amazonaws.com') !== -1) {
+if (packageLock.lockfileVersion !== 2) {
+  throw new Error('package-lock.json must have "lockfileVersion": 2');
+}
+
+Object.keys(packageLock.packages).forEach(dependencyName => {
+  const dependency = packageLock.packages[dependencyName];
+
+  if (dependencyName.includes('@cloudscape-design/')) {
+    delete packageLock.packages[dependencyName];
+  } else if (dependency.resolved && dependency.resolved.includes('codeartifact.us-west-2.amazonaws.com')) {
     throw Error('package-lock.json file contains a reference to CodeArtifact. Use regular npm to update the packages.');
   }
 });
