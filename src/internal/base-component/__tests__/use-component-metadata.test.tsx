8e7beb3113df6ea6cc5b86eb5343b9fe3ee38066
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-02-06 14:10:23 +0100

    Refactor use-component-metadata test

--- src/internal/base-component/__tests__/use-component-metadata.test.tsx
+++ src/internal/base-component/__tests__/use-component-metadata.test.tsx
@@ -14,16 +14,6 @@ test('should attach readonly metadata to the returned root DOM node', () => {
   const { container } = render(<TestComponent />);
   const rootNode: any = container.firstChild;
 
-  expect(rootNode[COMPONENT_METADATA_KEY]?.name).toBe('test-component');
-  expect(rootNode[COMPONENT_METADATA_KEY]?.version).toBe('3.0.0');
-
-  expect(() => {
-    rootNode[COMPONENT_METADATA_KEY]!.name = 'changed name';
-  }).toThrow(`Cannot assign to read only property 'name' of object '#<Object>'`);
-  expect(() => {
-    rootNode[COMPONENT_METADATA_KEY]!.version = 'changed version';
-  }).toThrow(`Cannot assign to read only property 'version' of object '#<Object>'`);
-
-  expect(rootNode[COMPONENT_METADATA_KEY]?.name).toBe('test-component');
-  expect(rootNode[COMPONENT_METADATA_KEY]?.version).toBe('3.0.0');
+  expect(rootNode[COMPONENT_METADATA_KEY]).toEqual({ name: 'test-component', version: '3.0.0' });
+  expect(Object.isFrozen(rootNode[COMPONENT_METADATA_KEY])).toBe(true);
 });
