b20767ae05ebf230f388d40a3b4855ccf0488112
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-02-08 14:49:29 +0100

    chore: Add generate-exports script for the package exports to work with TS

---
+++ scripts/generate-exports.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+const fs = require('fs');
+
+// This package exposes several APIs in different files. For each export we generate a separate file in the repository root
+const publicExports = ['dom', 'internal'];
+
+publicExports.forEach(exportName => {
+  fs.writeFileSync(`${exportName}.js`, `module.exports = require('./lib/${exportName}')`);
+  fs.writeFileSync(`${exportName}.d.ts`, `export * from './lib/${exportName}';`);
+});
