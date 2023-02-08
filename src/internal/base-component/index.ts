12ae4f4f55030006196cca4973f5798468934e5f
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-02-08 15:46:04 +0100

    chore: Add generate-exports script for the package exports to work wi… (#13)

--- src/internal/base-component/index.ts
+++
@@ -1,7 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-export { useComponentMetadata, COMPONENT_METADATA_KEY } from './component-metadata';
-export { useComponentMetrics } from './component-metrics';
-export { initAwsUiVersions } from './init-awsui-versions';
-export { Metrics } from './metrics/metrics';
---
+++ src/internal/index.ts
@@ -0,0 +1,7 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+export { useComponentMetadata, COMPONENT_METADATA_KEY } from './base-component/component-metadata';
+export { useComponentMetrics } from './base-component/component-metrics';
+export { initAwsUiVersions } from './base-component/init-awsui-versions';
+export { Metrics } from './base-component/metrics/metrics';
