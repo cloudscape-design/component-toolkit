54a09b3ac31af274bf55466c57d1f1973b69ab17
Andrei Zhaleznichenka <zhalea@amazon.de>
2023-07-06 12:24:13 +0200

    deduplicate interface

--- src/container-queries/interfaces.ts
+++ src/container-queries/interfaces.ts
@@ -1,10 +1,4 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
-export { ContainerQueryEntry } from '../internal/container-queries/interfaces';
-
-/**
- * React reference or element callback
- */
-export type ElementReference = (() => Element | null) | React.RefObject<Element>;
+export { ContainerQueryEntry, ElementReference } from '../internal/container-queries/interfaces';
