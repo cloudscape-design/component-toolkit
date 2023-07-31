24136e6ec96cd66f22d13e3e6747da82674da897
Boris Serdiuk <serdiuk@amazon.com>
2023-07-31 13:53:29 +0200

    feat: Upgrade nodeContains utility (#34)

--- src/dom/__tests__/node-contains.test.ts
+++ src/dom/__tests__/node-contains.test.ts
@@ -10,61 +10,53 @@ test('returns false if either node is null', () => {
   expect(nodeContains(null, null)).toBe(false);
 });
 
+// useful for passing `event.target` or `event.relatedTarget` without additional processing
+test('returns false if descendant is not a valid node', () => {
+  const div = document.createElement('div');
+  expect(nodeContains(div, window)).toBe(false);
+  expect(nodeContains(div, new EventTarget())).toBe(false);
+});
+
 const testCases: Record<string, string> = {
   regular: `<div id="parent">
-              <div id="inbetween">
-                <div id="child"></div>
-              </div>
-            </div>
+    <div id="inbetween">
+      <div id="child"></div>
+    </div>
+  </div>
 `,
-  svg: `<g id="parent">
-          <g id="inbetween">
-            <g id="child"></g>
-          </g>
-        </g>
+  svg: `<svg>
+    <g id="parent">
+      <g id="inbetween">
+        <g id="child"></g>
+      </g>
+    </g>
+  </svg>
 `,
   mixed: `<div id="parent">
-            <svg id="inbetween">
-              <g id="child"></g>
-            </svg>
-          </div>
+    <svg id="inbetween">
+      <g id="child"></g>
+    </svg>
+  </div>
 `,
 };
 
-[false, true].forEach(nativeContains => {
-  describe(`with${!nativeContains ? 'out' : ''} native .contains method`, () => {
-    let oldContains: Element['contains'];
+Object.keys(testCases).forEach(title => {
+  test(`finds ${title} nested nodes`, () => {
+    const div = document.createElement('div');
+    /* eslint-disable-next-line no-unsanitized/property */
+    div.innerHTML = testCases[title];
 
-    // The utility can use two possible algorithms: native .contains or a fallback approach using loops.
-    // In order to cover both use cases in the tests, we need to temporarily remove the contains function.
-    beforeAll(() => {
-      if (!nativeContains) {
-        oldContains = window.Element.prototype.contains;
-        (window.Element.prototype.contains as any) = undefined;
-      }
-    });
-    afterAll(() => {
-      if (!nativeContains) {
-        window.Element.prototype.contains = oldContains;
-      }
-    });
+    const parent = div.querySelector('#parent');
+    const inbetween = div.querySelector('#inbetween');
+    const child = div.querySelector('#child');
 
-    Object.keys(testCases).forEach(title => {
-      test(`finds ${title} nested nodes`, () => {
-        const div = document.createElement('div');
-        /* eslint-disable-next-line no-unsanitized/property */
-        div.innerHTML = testCases[title];
-
-        const parent = div.querySelector('#parent');
-        const inbetween = div.querySelector('#inbetween');
-        const child = div.querySelector('#child');
-
-        expect(nodeContains(parent, child)).toBe(true);
-        expect(nodeContains(parent, inbetween)).toBe(true);
-        expect(nodeContains(inbetween, child)).toBe(true);
-        expect(nodeContains(child, parent)).toBe(false);
-        expect(nodeContains(child, inbetween)).toBe(false);
-      });
-    });
+    expect(nodeContains(parent, parent)).toBe(true);
+    expect(nodeContains(child, child)).toBe(true);
+    expect(nodeContains(parent, child)).toBe(true);
+    expect(nodeContains(parent, inbetween)).toBe(true);
+    expect(nodeContains(inbetween, child)).toBe(true);
+    expect(nodeContains(child, parent)).toBe(false);
+    expect(nodeContains(child, inbetween)).toBe(false);
+    expect(nodeContains(inbetween, parent)).toBe(false);
   });
 });
