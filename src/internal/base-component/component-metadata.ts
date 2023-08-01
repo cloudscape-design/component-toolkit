5204df8aa1c15e3a100a8a37ce6901608702aefc
Connor Lanigan <dev@connorlanigan.com>
2023-08-01 11:12:37 +0200

    feat: Support `componentConfiguration` in the component metadata property (#35)

--- src/internal/base-component/component-metadata.ts
+++ src/internal/base-component/component-metadata.ts
@@ -5,10 +5,15 @@ import { useEffect, useRef } from 'react';
 
 export const COMPONENT_METADATA_KEY = '__awsuiMetadata__';
 
-export function useComponentMetadata<T = any>(componentName: string, packageVersion: string) {
+export function useComponentMetadata<T = any>(
+  componentName: string,
+  packageVersion: string,
+  componentConfiguration?: Record<string, any>
+) {
   interface AwsUiMetadata {
     name: string;
     version: string;
+    componentConfiguration?: Record<string, any>;
   }
 
   interface HTMLMetadataElement extends HTMLElement {
@@ -20,7 +25,7 @@ export function useComponentMetadata<T = any>(componentName: string, packageVers
   useEffect(() => {
     if (elementRef.current && !Object.prototype.hasOwnProperty.call(elementRef.current, COMPONENT_METADATA_KEY)) {
       const node = elementRef.current as unknown as HTMLMetadataElement;
-      const metadata = { name: componentName, version: packageVersion };
+      const metadata: AwsUiMetadata = { componentConfiguration, name: componentName, version: packageVersion };
 
       Object.freeze(metadata);
       Object.defineProperty(node, COMPONENT_METADATA_KEY, { value: metadata, writable: false });
