8e6caf639841a52300670a57a910798b31ceeda8
Abdallah AlHalees <abdallah.halees@gmail.com>
2023-12-07 17:34:24 +0100

    fix: check for descendant nodeType in nodeContains function (#44)

--- src/dom/node-contains.ts
+++ src/dom/node-contains.ts
@@ -7,7 +7,10 @@
  * @param descendant Node that is checked to be a descendant of the parent node
  */
 export default function nodeContains(parent: Node | null, descendant: Node | EventTarget | null) {
-  if (!parent || !descendant || !(descendant instanceof Node)) {
+  // ('nodeType' in descendant) is a workaround to check if descendant is a node
+  //  Node interface is tied to the window it's created in, if the descendant was moved to an iframe after it was created,
+  //  descendant instanceof Node will be false since Node has a different window
+  if (!parent || !descendant || !('nodeType' in descendant)) {
     return false;
   }
 
