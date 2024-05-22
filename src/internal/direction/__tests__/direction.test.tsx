70be9a63cf0d19929b9cd92e8aab42b083c72862
Scott O'Brien <scottob@amazon.com>
2024-05-22 14:47:53 -0400

    Test getLogicalBoundingClientRect.

--- src/internal/direction/__tests__/direction.test.tsx
+++ src/internal/direction/__tests__/direction.test.tsx
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
-import { getIsRtl, getScrollInlineStart } from '../index';
+import { getIsRtl, getLogicalBoundingClientRect, getScrollInlineStart } from '../index';
 
 describe('getIsRtl utility function', () => {
   test('detects direction of an ltr element', () => {
@@ -65,10 +65,78 @@ describe('getLogicalClientX utility function', () => {
 });
 */
 
-/*
 describe('getLogicalBoundingClientRect utility function', () => {
+  test('computes correct logical bounding client rect in ltr', () => {
+    const { container } = render(
+      <div id="container" style={{ width: '100px', overflowX: 'scroll' }}>
+        <div id="content" style={{ width: '400px' }}>
+          This is really really really really really really really really really really long content.
+        </div>
+      </div>
+    );
+
+    const element = container.querySelector('#content') as HTMLElement;
+
+    jest.spyOn(element, 'getBoundingClientRect').mockReturnValue({
+      bottom: 123,
+      height: 456,
+      left: 789,
+      right: 987,
+      toJSON() {
+        return;
+      },
+      top: 321,
+      width: 654,
+      x: 999,
+      y: 998,
+    });
+
+    expect(getLogicalBoundingClientRect(element)).toEqual({
+      blockSize: 456,
+      inlineSize: 654,
+      insetBlockEnd: 123,
+      insetBlockStart: 321,
+      insetInlineEnd: 1443,
+      insetInlineStart: 789,
+    });
+  });
+
+  test('computes correct logical bounding client rect in rtl', () => {
+    const { container } = render(
+      <div id="container" style={{ width: '100px', overflowX: 'scroll' }}>
+        <div id="content" style={{ width: '400px' }}>
+          This is really really really really really really really really really really long content.
+        </div>
+      </div>
+    );
+
+    const element = container.querySelector('#content') as HTMLElement;
+    element.style.direction = 'rtl';
+
+    jest.spyOn(element, 'getBoundingClientRect').mockReturnValue({
+      bottom: 123,
+      height: 456,
+      left: 789,
+      right: 987,
+      toJSON() {
+        return;
+      },
+      top: 321,
+      width: 654,
+      x: 999,
+      y: 998,
+    });
+
+    expect(getLogicalBoundingClientRect(element)).toEqual({
+      blockSize: 456,
+      inlineSize: 654,
+      insetBlockEnd: 123,
+      insetBlockStart: 321,
+      insetInlineEnd: -333,
+      insetInlineStart: -987,
+    });
+  });
 });
-*/
 
 /*
 describe('getLogicalPageX utility function', () => {
