06978adbd6b90a8652dc407675d84f2474a4f5a6
Francesco Longo <flongo@amazon.de>
2024-07-25 16:08:51 +0200

    chore: Export label identifier from analytics metadata

--- src/internal/analytics-metadata/index.ts
+++ src/internal/analytics-metadata/index.ts
@@ -1,7 +1,7 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-export { GeneratedAnalyticsMetadataFragment, GeneratedAnalyticsMetadata } from './interfaces';
+export { GeneratedAnalyticsMetadataFragment, GeneratedAnalyticsMetadata, LabelIdentifier } from './interfaces';
 export {
   getAnalyticsMetadataAttribute,
   copyAnalyticsMetadataAttribute,
