5a8a7826aa9806091a7635db41de43178e161d54
Mael Kerichard <maelker@amazon.com>
2025-11-24 11:47:34 +0000

    feat: add locale normalization and merging utilities
    Initially coming from https://github.com/cloudscape-design/components/tree/19cf57da0cefff1173ecf2d6457d84fb95b4a8df/src/internal/utils/locale

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@
 export { default as useContainerQuery } from './container-queries/use-container-query';
 export { default as useControllableState } from './use-controllable-state/use-controllable-state';
 
-export type { PropertyDescriptions } from './use-controllable-state/interfaces';
 export type { ContainerQueryEntry } from './container-queries/interfaces';
+export * from './locale';
+export type { PropertyDescriptions } from './use-controllable-state/interfaces';
